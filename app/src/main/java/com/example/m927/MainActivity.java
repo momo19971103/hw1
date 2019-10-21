e3d7d17326410b199e8aabbd3d717aedce8879c0
momo19971103 <momo19971103@yahoo.com.tw>
2019-10-21 14:16:56 +0800

    增加註冊鈕

--- app/src/main/java/com/example/m927/MainActivity.java
+++ app/src/main/java/com/example/m927/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bbb = (Button)findViewById(R.id.B_login);
+        bbb = (Button) findViewById(R.id.B_login);
 
 
     }
